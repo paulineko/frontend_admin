a26209fb8c29c7d128bdbc606855c26ac193b597
paulineko <pauline17042002@gmail.com>
2022-08-07 21:34:40 +0400

    start project

---
+++ src/pages/authorization.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import logotip from '../img/logo.png';
+import login from '../img/login.png';
+import password from '../img/password.png';
+
+const authorization = () => {
+    return (
+      <div>
+  <main>
+      
+  
+           <h1>Краеведческий Музей</h1>
+   
+  
+       
+        </main>
+  
+      </div>
+    )
+  }
+  
+  export default authorization
