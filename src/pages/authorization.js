f1598f642e014b61c626edeb1debd73618aa6de2
paulineko <pauline17042002@gmail.com>
2022-08-09 15:15:02 +0400

    autorization pages

--- src/pages/authorization.js
+++ src/pages/authorization.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import logotip from '../img/logo.png';
+
+import Main1 from '../img/main_1.png';
+import Main2 from '../img/main_2.png';
 import login from '../img/login.png';
 import password from '../img/password.png';
 
@@ -8,9 +10,27 @@ const Authorization = () => {
       <div>
   <main>
       
-    <img className='photo' src={logotip} width="539" height='329' fluid />
-           <h1>Краеведческий Музей</h1>
-           <h1>Краеведческий Музей</h1>
+   <div class = "container-authorization">
+   <div className='row-rast'>
+         <div className='row-col1'>
+           <div className="mobile-student">MOBILE <br/><span class="colortext"> STUDENT</span>  </div>
+
+
+           <div className="recover-password">Забыли пароль?</div>
+           <div>
+           <button className ='button1' >Войти</button>
+           </div>
+           <div>
+           <button className ='button1' >Зарегистрироваться</button>
+           </div>
+         </div>
+         <div className='row-col2'>
+         <img className="figures1" src={Main2} alt="logo"  />
+         <img className="figures2" src={Main1} alt="logo"  />
+         
+         </div>
+        </div>
+   </div>
   
        
         </main>
