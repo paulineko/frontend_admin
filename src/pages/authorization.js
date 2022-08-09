0cb38390924d77638d31b996f98a01922430bf41
NatawaSergeevna <nett0daynet@gmail.com>
2022-08-09 16:06:11 +0400

    text logo
    text logo

--- src/pages/authorization.js
+++ src/pages/authorization.js
@@ -8,33 +8,29 @@ import password from '../img/password.png';
 const Authorization = () => {
     return (
       <div>
-  <main>
-      
-   <div class = "container-authorization">
-   <div className='row-rast'>
-         <div className='row-col1'>
-           <div className="mobile-student">MOBILE <br/><span class="colortext"> STUDENT</span>  </div>
-
-
-           <div className="recover-password">Забыли пароль?</div>
-           <div>
-           <button className ='button1' >Войти</button>
-           </div>
-           <div>
-           <button className ='button1' >Зарегистрироваться</button>
-           </div>
-         </div>
-         <div className='row-col2'>
-         <img className="figures1" src={Main2} alt="logo"  />
-         <img className="figures2" src={Main1} alt="logo"  />
-         
-         </div>
-        </div>
-   </div>
-  
-       
+        <main>
+          <div class = "container-authorization">
+            <div className='row-rast'>
+              <div className='row-col1'>
+                <div className="mobile-student">MOBILE 
+                  <br/>
+                  <span class="colortext"> STUDENT</span>  
+                </div>
+                <div className="recover-password">Забыли пароль?</div>
+                <div>
+                  <button className ='button1' >Войти</button>
+                </div>
+                <div>
+                  <button className ='button1' >Зарегистрироваться</button>
+                  </div>
+              </div>
+              <div className='row-col2'>
+                <img className="figures1" src={Main2} alt="logo"  />
+                <img className="figures2" src={Main1} alt="logo"  />
+              </div>
+            </div>
+          </div>
         </main>
-  
       </div>
     )
   }
