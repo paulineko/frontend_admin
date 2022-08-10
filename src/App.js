21cfdba7397259355b96d95d931c7bd7975facee
paulineko <pauline17042002@gmail.com>
2022-08-10 12:39:44 +0400

    reg

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Authorization from "./pages/authorization.js";
 import Header from './pages/header';
+import Registration from './pages/registration';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Authorization/>
+      <Registration/>
     </div>
   );
 }
