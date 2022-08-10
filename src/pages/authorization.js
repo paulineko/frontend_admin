29dd4c26c7ede8d9cdd5ce6073093bd65ffa27a4
NatawaSergeevna <nett0daynet@gmail.com>
2022-08-10 12:30:07 +0400

    added form
    added form from formik

--- src/pages/authorization.js
+++ src/pages/authorization.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";  
+import { useFormik } from 'formik';
 
 import Main1 from '../img/main_1.png';
 import Main2 from '../img/main_2.png';
-import login from '../img/login.png';
-import password from '../img/password.png';
 
 const Authorization = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
     return (
       <div>
         <main>
@@ -16,13 +25,39 @@ const Authorization = () => {
                   <br/>
                   <span class="colortext"> STUDENT</span>  
                 </div>
-                <div className="recover-password">Забыли пароль?</div>
-                <div>
-                  <button className ='button1' >Войти</button>
-                </div>
-                <div>
-                  <button className ='button1' >Зарегистрироваться</button>
+                <form className="formAutorization" onSubmit={formik.handleSubmit}>
+                  <div className='login-autorization'>
+                    <input
+                      className='login-autorization-input'
+                      id="email"
+                      name="email"
+                      type="email"
+                      size="26"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                      placeholder="Email"
+                    />
                   </div>
+                  <div className='password-autorization'>
+                    <input
+                      className='password-autorization-input'
+                      id="password"
+                      name="password"
+                      type="password"
+                      size="26"
+                      onChange={formik.handleChange}
+                      value={formik.values.password}
+                      placeholder="Пароль"
+                    />
+                  </div>
+                  <div className="recover-password">Забыли пароль?</div>
+                  <div>
+                    <button className ='button1' type="submit">Войти</button>
+                  </div>
+                  <div>
+                    <button className ='button1' >Зарегистрироваться</button>
+                  </div>
+                </form>
               </div>
               <div className='row-col2'>
                 <img className="figures1" src={Main1} alt="logo"  />
@@ -34,6 +69,6 @@ const Authorization = () => {
         </main>
       </div>
     )
-  }
+}
   
   export default Authorization
